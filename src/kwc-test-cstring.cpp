c959bc4e468e95419e7bccf6527d5675867f7d0b
Kowloon walled city (coin) <kwccoin@users.noreply.github.com>
2023-11-15 11:28:26 +0800

    It does not work at all in M1 at least ...

---
+++ src/kwc-test-cstring.cpp
@@ -0,0 +1,22 @@
+// kwc see https://stackoverflow.com/questions/4073289/compiling-simple-hello-world-program-on-os-x-via-command-line
+//
+
+//#include <iostream>
+//#include <string>
+
+#include <stdio.h>
+#include <ctime>
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+    cout << "hello world" << endl;
+
+    return 1;
+}
